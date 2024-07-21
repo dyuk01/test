5330ce9adc9c154eaf819e25ae12d163adac281c
Peter Yuk <peteryuk91@gmail.com>
2024-07-21 15:43:16 +0900

    map logic update

--- js/index.js
+++ js/index.js
@@ -24,21 +24,18 @@ document.addEventListener('DOMContentLoaded', function() {
     var player;
     var cursors;
     var map;
-    // Layers
     var layers = {};
 
     function preload() {
         console.log("preload");
-        // Load player spritesheet
+
         this.load.spritesheet('player', 'img/spritesheet/player_movement.png', {
             frameWidth: 48,
             frameHeight: 48
         });
 
-        // Load the Tiled map JSON
         this.load.tilemapTiledJSON('map', 'asset/map/main_map.tmj');
 
-        // Load all tileset images used in the map
         this.load.image('tile_cave_floor', 'asset/map/tileset/cave_floor.png');
         this.load.image('tile_cave_ladder', 'asset/map/tileset/cave_ladder.png');
         this.load.image('tile_cave_wall', 'asset/map/tileset/cave_wall.png');
@@ -56,108 +53,136 @@ document.addEventListener('DOMContentLoaded', function() {
     function create() {
         console.log("create");
 
-        // Create the map
-        map = this.make.tilemap({ key: 'map' });
-        console.log("Map loaded:", map);
+        try {
+            map = this.make.tilemap({ key: 'map' });
+            console.log("Map loaded:", map);
 
-        // Add each tileset image to the map
-        var caveFloorTileset = map.addTilesetImage('cave_floor', 'tile_cave_floor');
-        var caveLadderTileset = map.addTilesetImage('cave_ladder', 'tile_cave_ladder');
-        var caveWallTileset = map.addTilesetImage('cave_wall', 'tile_cave_wall');
-        var farmTileset = map.addTilesetImage('farm', 'tile_farm');
-        var furnitureTileset = map.addTilesetImage('furniture', 'tile_furniture');
-        var grassTileset = map.addTilesetImage('grass', 'tile_grass');
-        var hillsTileset = map.addTilesetImage('hills', 'tile_hills');
-        var houseTileset = map.addTilesetImage('house', 'tile_house');
-        var pathTileset = map.addTilesetImage('path', 'tile_path');
-        var treeTileset = map.addTilesetImage('tree', 'tile_tree');
-        var waterTileset = map.addTilesetImage('water', 'tile_water');
-        var rockTileset = map.addTilesetImage('rock', 'tile_rock');
+            var caveFloorTileset = map.addTilesetImage('cave_floor', 'tile_cave_floor');
+            var caveLadderTileset = map.addTilesetImage('cave_ladder', 'tile_cave_ladder');
+            var caveWallTileset = map.addTilesetImage('cave_wall', 'tile_cave_wall');
+            var farmTileset = map.addTilesetImage('farm', 'tile_farm');
+            var furnitureTileset = map.addTilesetImage('furniture', 'tile_furniture');
+            var grassTileset = map.addTilesetImage('grass', 'tile_grass');
+            var hillsTileset = map.addTilesetImage('hills', 'tile_hills');
+            var houseTileset = map.addTilesetImage('house', 'tile_house');
+            var pathTileset = map.addTilesetImage('path', 'tile_path');
+            var treeTileset = map.addTilesetImage('tree', 'tile_tree');
+            var waterTileset = map.addTilesetImage('water', 'tile_water');
+            var rockTileset = map.addTilesetImage('rock', 'tile_rock');
 
-        console.log("Tilesets loaded:", {
-            caveFloorTileset,
-            caveLadderTileset,
-            caveWallTileset,
-            farmTileset,
-            furnitureTileset,
-            grassTileset,
-            hillsTileset,
-            houseTileset,
-            pathTileset,
-            treeTileset,
-            waterTileset,
-            rockTileset
-        });
+            console.log("Tilesets loaded:", {
+                caveFloorTileset,
+                caveLadderTileset,
+                caveWallTileset,
+                farmTileset,
+                furnitureTileset,
+                grassTileset,
+                hillsTileset,
+                houseTileset,
+                pathTileset,
+                treeTileset,
+                waterTileset,
+                rockTileset
+            });
 
-        // Create each layer and store them in the layers object
-        layers['water'] = map.createLayer('water', [waterTileset], 0, 0);
-        layers['land'] = map.createLayer('land', [grassTileset], 0, 0);
-        layers['land_deco'] = map.createLayer('land_deco', [grassTileset], 0, 0);
-        layers['house_floor'] = map.createLayer('house_floor', [houseTileset, pathTileset], 0, 0);    
-        layers['house_wall'] = map.createLayer('house_wall', [houseTileset], 0, 0);    
-        layers['hill'] = map.createLayer('hill', [hillsTileset], 0, 0);
-        layers['trees'] = map.createLayer('trees', [treeTileset], 0, 0);       
-        layers['farm'] = map.createLayer('farm', [farmTileset], 0, 0);  
-        layers['mine'] = map.createLayer('mine', [caveLadderTileset], 0, 0);   
-        layers['plants'] = map.createLayer('plants', [grassTileset, treeTileset], 0, 0);   
-        layers['furniture'] = map.createLayer('furniture', [furnitureTileset], 0, 0);    
+            layers['water'] = map.createLayer('water', [waterTileset], 0, 0);
+            console.log("Water layer created:", layers['water']);
 
-        console.log("Layers created:", layers);
+            layers['land'] = map.createLayer('land', [grassTileset], 0, 0);
+            console.log("Land layer created:", layers['land']);
 
-        // Set collisions for layers if needed
-        Object.values(layers).forEach(layer => {
-            layer.setCollisionByExclusion([-1]);
-            console.log("Collision set for layer:", layer);
-        });
+            layers['land_deco'] = map.createLayer('land_deco', [grassTileset], 0, 0);
+            console.log("Land Deco layer created:", layers['land_deco']);
 
-        // Create the player
-        player = this.physics.add.sprite(400, 300, 'player');
-        player.setCollideWorldBounds(true);
-        console.log("Player created:", player);
+            layers['house_floor'] = map.createLayer('house_floor', [houseTileset, pathTileset], 0, 0);
+            console.log("House Floor layer created:", layers['house_floor']);
 
-        // Enable collision between the player and the layers
-        Object.values(layers).forEach(layer => {
-            this.physics.add.collider(player, layer);
-            console.log("Collider added for layer:", layer);
-        });
+            layers['house_wall'] = map.createLayer('house_wall', [houseTileset], 0, 0);
+            console.log("House Wall layer created:", layers['house_wall']);
 
-        // Create player animations
-        this.anims.create({
-            key: 'moveSouth',
-            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
-            frameRate: 10,
-            repeat: -1
-        });
+            layers['hill'] = map.createLayer('hill', [hillsTileset], 0, 0);
+            console.log("Hill layer created:", layers['hill']);
 
-        this.anims.create({
-            key: 'moveNorth',
-            frames: this.anims.generateFrameNumbers('player', { start: 4, end: 7 }),
-            frameRate: 10,
-            repeat: -1
-        });
+            layers['trees'] = map.createLayer('trees', [treeTileset], 0, 0);
+            console.log("Trees layer created:", layers['trees']);
 
-        this.anims.create({
-            key: 'moveWest',
-            frames: this.anims.generateFrameNumbers('player', { start: 8, end: 11 }),
-            frameRate: 10,
-            repeat: -1
-        });
+            layers['farm'] = map.createLayer('farm', [farmTileset], 0, 0);
+            console.log("Farm layer created:", layers['farm']);
 
-        this.anims.create({
-            key: 'moveEast',
-            frames: this.anims.generateFrameNumbers('player', { start: 12, end: 15 }),
-            frameRate: 10,
-            repeat: -1
-        });
+            layers['mine'] = map.createLayer('mine', [caveLadderTileset], 0, 0);
+            console.log("Mine layer created:", layers['mine']);
 
-        // Set up cursor keys
-        cursors = this.input.keyboard.addKeys({
-            up: Phaser.Input.Keyboard.KeyCodes.W,
-            down: Phaser.Input.Keyboard.KeyCodes.S,
-            left: Phaser.Input.Keyboard.KeyCodes.A,
-            right: Phaser.Input.Keyboard.KeyCodes.D
-        });
-        console.log("Cursors set up:", cursors);
+            layers['plants'] = map.createLayer('plants', [grassTileset, treeTileset], 0, 0);
+            console.log("Plants layer created:", layers['plants']);
+
+            layers['furniture'] = map.createLayer('furniture', [furnitureTileset], 0, 0);
+            console.log("Furniture layer created:", layers['furniture']);
+
+            // Set collisions for layers if needed
+            Object.values(layers).forEach(layer => {
+                if (layer) {
+                    layer.setCollisionByExclusion([-1]);
+                    console.log("Collision set for layer:", layer);
+                } else {
+                    console.log("Layer is undefined:", layer);
+                }
+            });
+
+            // Create the player
+            player = this.physics.add.sprite(400, 300, 'player');
+            player.setCollideWorldBounds(true);
+            console.log("Player created:", player);
+
+            // Enable collision between the player and the layers
+            Object.values(layers).forEach(layer => {
+                if (layer) {
+                    this.physics.add.collider(player, layer);
+                    console.log("Collider added for layer:", layer);
+                } else {
+                    console.log("Layer is undefined:", layer);
+                }
+            });
+
+            // Create player animations
+            this.anims.create({
+                key: 'moveSouth',
+                frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+                frameRate: 10,
+                repeat: -1
+            });
+
+            this.anims.create({
+                key: 'moveNorth',
+                frames: this.anims.generateFrameNumbers('player', { start: 4, end: 7 }),
+                frameRate: 10,
+                repeat: -1
+            });
+
+            this.anims.create({
+                key: 'moveWest',
+                frames: this.anims.generateFrameNumbers('player', { start: 8, end: 11 }),
+                frameRate: 10,
+                repeat: -1
+            });
+
+            this.anims.create({
+                key: 'moveEast',
+                frames: this.anims.generateFrameNumbers('player', { start: 12, end: 15 }),
+                frameRate: 10,
+                repeat: -1
+            });
+
+            // Set up cursor keys
+            cursors = this.input.keyboard.addKeys({
+                up: Phaser.Input.Keyboard.KeyCodes.W,
+                down: Phaser.Input.Keyboard.KeyCodes.S,
+                left: Phaser.Input.Keyboard.KeyCodes.A,
+                right: Phaser.Input.Keyboard.KeyCodes.D
+            });
+            console.log("Cursors set up:", cursors);
+        } catch (error) {
+            console.error("An error occurred during create:", error);
+        }
     }
 
     function update() {
@@ -165,24 +190,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (cursors.left.isDown) {
             player.setVelocityX(-160);
-            if (player.anims.currentAnim !== this.anims.get('moveWest')) {
-                player.anims.play('moveWest', true);
-            }
+            player.anims.play('moveWest', true);
         } else if (cursors.right.isDown) {
             player.setVelocityX(160);
-            if (player.anims.currentAnim !== this.anims.get('moveEast')) {
-                player.anims.play('moveEast', true);
-            }
+            player.anims.play('moveEast', true);
         } else if (cursors.up.isDown) {
             player.setVelocityY(-160);
-            if (player.anims.currentAnim !== this.anims.get('moveNorth')) {
-                player.anims.play('moveNorth', true);
-            }
+            player.anims.play('moveNorth', true);
         } else if (cursors.down.isDown) {
             player.setVelocityY(160);
-            if (player.anims.currentAnim !== this.anims.get('moveSouth')) {
-                player.anims.play('moveSouth', true);
-            }
+            player.anims.play('moveSouth', true);
         } else {
             player.anims.stop();
         }
