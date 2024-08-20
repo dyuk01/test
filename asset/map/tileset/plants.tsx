2914ca089f0c2287af04e95f97e80a891a5767a5
Peter Yuk <peteryuk91@gmail.com>
2024-08-20 09:18:30 -0400

    plant feature update

---
+++ asset/map/tileset/plants.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="plants" tilewidth="16" tileheight="16" tilecount="75" columns="5">
+ <image source="../../../../../../Downloads/Sprout Lands - Sprites - premium pack/Objects/Farming Plants.png" trans="a1a1a1" width="80" height="240"/>
+</tileset>
