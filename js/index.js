06bec34903c61ed26cb85991ce620aaeeddf525e
Peter Yuk <peteryuk91@gmail.com>
2024-08-22 17:17:56 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -193,16 +193,12 @@ document.addEventListener('DOMContentLoaded', function() {
             
                     // Add collider
                     this.physics.add.collider(player, obstacle);
-            
-                    // // Optional: Visualize the collision rectangles to ensure correct alignment
-                    // this.add.graphics()
-                    //     .lineStyle(2, 0xff0000, 1)
-                    //     .strokeRect(x - originAdjustmentX, y - originAdjustmentY, width, height);
+
+
+                    // Set up inventory UI
+                    createInventoryUI.call(this);
                 }, this);
             }
-
-            // Set up inventory UI
-            createInventoryUI.call(this);
 
             // Create player animations
             this.anims.create({
