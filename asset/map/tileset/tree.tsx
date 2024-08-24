ff1bdb2819e4a1ff73dce1a55f8feda48fceb038
Peter Yuk <peteryuk91@gmail.com>
2024-08-24 14:01:38 -0400

    update

---
+++ asset/map/tileset/tree.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tree" tilewidth="16" tileheight="16" tilecount="45" columns="9">
+ <image source="tree.png" trans="a1a1a1" width="144" height="80"/>
+ <tile id="9">
+  <objectgroup draworder="index">
+   <object id="1" x="4.08493" y="4.93991" width="7.74236" height="7.12488"/>
+   <object id="3" x="2.84995" y="1.85247" width="10.2598" height="3.03995"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="1" x="12.0648" y="7.88486" width="3.84743" height="7.07738"/>
+   <object id="2" x="10.9723" y="12.0173" width="1.37748" height="0.854985"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0474992" y="8.07486" width="4.03743" height="6.79238"/>
+   <object id="2" x="3.98993" y="12.0173" width="0.997483" height="0.902484"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="2" x="11.9698" y="7.93236" width="4.08493" height="6.93488"/>
+   <object id="3" x="11.0198" y="11.9223" width="1.04498" height="0.902484"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0949983" y="7.93236" width="3.84743" height="6.98238"/>
+   <object id="2" x="3.94243" y="12.0173" width="0.997483" height="0.807486"/>
+  </objectgroup>
+ </tile>
+</tileset>
