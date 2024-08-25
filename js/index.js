bbb7c7b049a77b1c7f8fde5b7ad73d7dd4d4376e
Peter Yuk <peteryuk91@gmail.com>
2024-08-25 19:38:23 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -285,7 +285,7 @@ document.addEventListener('DOMContentLoaded', function() {
         
         // Define the slot size and position within the inventory bar
         const slotSize = 22 * scaleFactor; // Scale down the slot size
-        const padding = 2 * scaleFactor; // Scale down the padding
+        const padding = 3 * scaleFactor; // Scale down the padding
         const startX = inventoryBar.x - (inventoryBar.displayWidth / 2) + slotSize / 2 + 3; // Start position for slots
         const startY = inventoryBar.y + inventoryBar.displayHeight / 2;
         
